0effd30bd1006ec4db47dee4047d2efbac1d466d
Diego Rocha <diegorsf.dns@gmail.com>
2022-12-19 11:09:16 -0300

    feature: Add user registration to Careers

--- desafio.kt
+++ desafio.kt
@@ -1,13 +1,25 @@
+import DIOEducationalContent.getSingleContentCollection
+import DIOEducationalContent.retrieveAllEducationalContentAsList
 
+const val ANDROID_DEVELOPER = 0
+const val FRONTEND_DEVELOPER = 1
+const val UNITY3D_DEVELOPER = 2
 enum class ContentLevel { BASIC, INTERMEDIATE, ADVANCED }
 
-data class Formation(
-    val formationName: String,
-    var content: MutableList<EducationalContent>
-) {
+class AndroidCareer(
+    careerName: String = "Android Developer",
+    content: MutableList<EducationalContent>? = null
+) : Career(
+    careerName,
+    content
+)
 
-    private val educationalContentList: List<String>
-        get() = content.map { it.toString() }
+open class Career(
+     var careerName: String? = "",
+     var content: MutableList<EducationalContent>? = null
+) {
+    // TODO: Implement this property
+    val currentStack: List<String>? get() = content?.map { it.toString() }
 
     private val enrolledStudents = mutableListOf<User>()
 
@@ -24,22 +36,24 @@ data class EducationalContent(
     val contentDuration: Int
 ) {
 
-    private val mContentLevel: String get() = when(contentLevel) {
+    // TODO: Implement these properties
+     val mContentLevel: String get() = when(contentLevel) {
         ContentLevel.BASIC -> "Basic"
         ContentLevel.INTERMEDIATE -> "Intermediate"
         ContentLevel.ADVANCED -> "Advanced"
         else -> ""
     }
+    
+    val mContentDuration: String get() = "${ contentDuration / 60 } hour(s)"
 }
 
 // A User only needs a name to get it started
-data class User(val name: String, var userFormation: MutableList<Formation>? = null)
+data class User(val name: String, var userEducation: MutableList<Career>? = null)
 
-// Enrolled Formations can be accessed by extension property and by extension function
-val User.allFormations: String get() = userFormation?.joinToString { it.formationName }.toString()
-//fun User.getStudentFormations(): String = userFormation?.joinToString { it.formationName }.toString()
+// Enrolled Careers can be accessed by extension property and by extension function
+val User.enrolledContent: String get() = userEducation?.joinToString { it.careerName!! }.toString()
+//fun User.getStudentFormations(): String = userEducation?.joinToString { it.careerName!! }.toString()
 
-//
 object DIOEducationalContent {
     private val educationalContent = mapOf(
         "android_developer" to mutableListOf(
@@ -62,15 +76,32 @@ object DIOEducationalContent {
         )
     )
 
-    fun retrieveAllEducationalContent() : MutableList<EducationalContent> {
-        return educationalContent.get(key = "android_developer") !!
+    fun getContentKeys() : List<String> {
+        val contentKeys = arrayListOf<String>()
+
+        educationalContent.onEach { item ->
+            contentKeys.add(item.key)
+        }
+        return contentKeys
+    }
+
+    fun retrieveAllEducationalContentAsList(): List<String> {
+        val allEducationalContent = ArrayList<String>()
+
+        educationalContent.map { entry ->
+            entry.value.map { educationalContent ->
+                allEducationalContent.add(educationalContent.contentName)
+            }
+        }
+        return allEducationalContent
     }
 
     fun getSingleContentCollection(contentKey: String) : MutableList<EducationalContent> {
         try {
             return educationalContent[contentKey]!!
+
         } catch (e: NullPointerException) {
-            println("${e.message} : Educational Content could not be found. Did you type the \"contentKey\" correctly?")
+            println("${e.message} : Educational Content could not be found. Did you enter the correct \"contentKey\"?")
         }
         return emptyList<EducationalContent>().toMutableList()
     }
@@ -78,48 +109,68 @@ object DIOEducationalContent {
 
 fun main() {
 
-    val androidDevelopment  = mutableListOf(
-        EducationalContent("Kotlin", ContentLevel.INTERMEDIATE, 120),
-        EducationalContent("GIT", ContentLevel.BASIC, 60),
-        EducationalContent("Tests", ContentLevel.ADVANCED, 180),
+    val contentKeys = DIOEducationalContent.getContentKeys()
+    val androidDevelopment  = getSingleContentCollection(contentKeys[ANDROID_DEVELOPER])
+    val frontEndDevelopment  = getSingleContentCollection(contentKeys[FRONTEND_DEVELOPER])
+    val unity3dDeveloper = getSingleContentCollection(contentKeys[UNITY3D_DEVELOPER])
+
+    /*  TEST CASES */
+
+    // A single Career
+    var currentCareer = registerCareerAs(contentKeys[ANDROID_DEVELOPER], AndroidCareer())
+    // currentCareer = registerCareerAs(contentKeys[ANDROID_DEVELOPER], Career())       // "AndroidCareer" can be
+                                                                                       // replaced with "Career"
+
+    // There can be many careers available
+    val availableCareers = mutableListOf(
+        Career(careerName = getCareerNameBy(contentKeys[ANDROID_DEVELOPER]), content = androidDevelopment),
+        Career(careerName = getCareerNameBy(contentKeys[FRONTEND_DEVELOPER]), content = frontEndDevelopment),
+        Career(careerName = getCareerNameBy(contentKeys[UNITY3D_DEVELOPER]), content = unity3dDeveloper)
     )
 
-    // There can be many formations available
-    val formations = mutableListOf(
-        Formation(formationName = "Android", content = androidDevelopment),
-        Formation(formationName = "Web Developer", content = androidDevelopment)
-    )
+    val student = User(name = "Diego")                      // A user that hasn't enrolled in any career
+     //  .onlyStudies(currentCareer)                       // Enrolled in a single formation
+     student.onlyStudies(currentCareer)                   // You can also call on the instance
 
-    // Fail safe - Single Formation
-    val educationalContent = DIOEducationalContent.getSingleContentCollection("android_developer")
-    val androidFormation = Formation(formationName = "Android Developer", content = educationalContent)
+   // student.studies(availableCareers)                  // Enrolled in several careers
 
-    // Test Cases
-    // A user that is not enrolled in any formation
-    val student = User(name = "Diego")
-    //   .onlyStudies(androidFormation)           // Enrolled in a single formation
-    // student.onlyStudies(androidFormation)       // You can also call on the instance
+    // Which careers the student is currently enrolled in
+    println("${student.name} is enrolled in: ${student.enrolledContent}" )
 
-    // Enrolled in several formations
-    student.studies(formations)
-
-    // What formations the student is currently enrolled in
-    println("${student.name} is enrolled in: ${student.allFormations}" )
-
+    val allContent = retrieveAllEducationalContentAsList().joinToString { it }
+   // println( "A list of All content available:\n$allContent")     // All Educational Content available
 }
 
-fun User.onlyStudies(singleFormation: Formation): User{
+private fun registerCareerAs(careerKey: String, career: Career): Career{
+
+    val educationalContent = DIOEducationalContent.getSingleContentCollection(careerKey)
+    return career.apply {
+        careerName = getCareerNameBy(careerKey)
+        content = educationalContent
+    }
+}
+
+private fun getCareerNameBy(key: String) : String{
+    return when (key){
+        "android_developer" -> "Android Developer"
+        "front_end_developer" -> "Front-End Developer"
+        "unity_3d_game_developer" -> "Unity 3D Game Developer"
+        else -> "NO EDUCATION"
+    }
+}
+
+private fun User.onlyStudies(singleFormation: Career): User{
 
     return apply {
-        this.userFormation = mutableListOf(singleFormation)
+        this.userEducation = mutableListOf(singleFormation)
         singleFormation.receives(this)
     }
 }
 
-fun User.studies(formation: MutableList<Formation>) : User{
+private fun User.studies(formation: MutableList<Career>) : User{
 
     return apply {
-        this.userFormation = formation
+        this.userEducation = formation
 
         formation.forEach { enrolledFormation ->
             enrolledFormation.receives(this)
